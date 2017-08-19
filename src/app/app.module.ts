91a848ea8007e6f8231d6bd0f87fd46ae30e777c
Zamay <mityazamay@gmail.com>
2017-08-19 10:53:44 +0300

    Why is not working ?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NavModule}      from './nav/nav.module';
+import {HeaderModule}   from './header/header.module';
+import {CommonModule} from "@angular/common";
+import {MainModule} from "./main/main.module";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    NavModule,
+    MainModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
